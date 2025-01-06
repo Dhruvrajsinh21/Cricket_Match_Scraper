f20bdfc3899b7ee09b2818a6b4bb11e81431bedf
Dhruvrajsinh Solanki <115185535+Dhruvrajsinh21@users.noreply.github.com>
2025-01-06 08:27:02 +0530

    Update notify.py

--- websock/notify.py
+++ websock/notify.py
@@ -2,5 +2,5 @@ from websock.server import notify_clients
 import json
 
 async def send_match_update():
-    update = {"match_id": 1, "data": "hi"}
+    update = {"match_id": 1, "data": "match"}
     await notify_clients(json.dumps(update))
